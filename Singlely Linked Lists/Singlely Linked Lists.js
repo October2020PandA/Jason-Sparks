96683327118c31e3f71da1c6444d853e3d3955c4
Jason Sparks <hijasonsparks@gmail.com>
2020-10-19 17:03:46 -0700

    Added: Singlely Linked Lists Fronts assignment

--- Singlely Linked Lists/Singlely Linked Lists.js
+++ Singlely Linked Lists/Singlely Linked Lists.js
@@ -1,44 +1,56 @@
-class Node{
-    constructor(value){
+// Singlely linked list node class
+class Node {
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
 
+// Creating a few instances of singlely linked list nodes
 var first_node = new Node("Mike");
 var second_node = new Node("Shannon");
 var third_node = new Node("William");
 
-class SLL{
-    constructor(){
-        this.head = null;
+// Singlely linked list class
+class SLL {
+    constructor() {
+        this.head = null; // A singlely linked list class only has one attribute which is a single node, we call it "head" here. 
     }
 
-    // Given a value, create a new node instance and set it to the head of the SLL
-    addToFront(value){
-        var new_node = new Node(value);
+    // Singlely linked list class methods 
+    // A method for adding a node to the front of our list given a value
+    addToFront(value) {
+        var new_node = new Node(value); // create a new node instance
         // Scenario 1 - the list is empty
         if(this.head == null){
-            this.head = new_node; // this.head = new Node(value);
+            this.head = new_node;  // assign the new node instance as the singlely linked list's first node
             return this;
         }
 
         // Scenario 2 - the list is not empty 
-        new_node.next = this.head; // Store the current head so that it isn't lost
-        this.head = new_node // Change which node is the "head" of the list
+        new_node.next = this.head; // Store the current first node as the next node so that it isn't lost
+        this.head = new_node       // Change which node is the "head" of the list
         return this;
     }
 
-    displayList(){
+    removeFront() {
+        if(this.head) {
+            this.head = this.head.next;
+        }
+        return this.head;
+    }
+    
+    // A method for viewing our singlely linked list
+    displayList() {
         if(this.head == null){
             console.log(this);
             return this;
         }
-        var runner = this.head; //this is a node object at the start of the list
+        var currentNode = this.head; //this is a node object at the start of the list
         var list = "";
-        while(runner != null){
-            list = list+`Node: ${runner.value} -> `; // console.log something helpful to show what is in the node
-            runner = runner.next // Update the runner to the next node in the list            
+        while(currentNode != null){
+            list = list + `Current node's value: ${currentNode.value} -> \n`; 
+            currentNode = currentNode.next // Move on to the next node in the list            
         }
         console.log(list);
         return this;
@@ -46,10 +58,14 @@ class SLL{
 }
 
 var my_sll = new SLL();
-console.log(my_sll);
-console.log(my_sll.addToFront("Amanda"));
-console.log(my_sll.addToFront("Bob"));
-console.log(my_sll.addToFront("Sue"));
-console.log(my_sll.addToFront("Victor"));
-console.log(my_sll.addToFront("Shannon"));
+
+my_sll.addToFront("Amanda");
+my_sll.addToFront("Bob");
+my_sll.addToFront("Sue");
+my_sll.addToFront("Victor");
+my_sll.addToFront("Shannon");
+
+my_sll.displayList();
+
+my_sll.removeFront();
 my_sll.displayList();
